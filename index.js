027f9c1f6832098e50f090590165be507e2cc5ea
Artem Nguyen <chunguen@vk.com>
2023-04-22 13:40:07 +0300

    add burger menu

--- index.js
+++ index.js
@@ -50,3 +50,33 @@ new Accordion ('.faq__accordion--container', {
   panelClass: 'faq__accordion--panel',
   activeClass: 'faq__accordion--active'
 });
+
+
+//burger menu
+
+const burger = document.querySelector('.header__burger--btn');
+const menu = document.querySelector('.header__nav');
+const menuLinks = menu.querySelectorAll('.header__nav--item .header__nav--link');
+
+burger.addEventListener('click',
+
+    function () {
+
+        burger.classList.toggle('header__burger--line--active');
+
+        menu.classList.toggle('header__nav--active');
+
+        document.body.classList.toggle('header__stop--scroll');
+    })
+
+menuLinks.forEach(function (el) {
+    el.addEventListener('click', function () {
+
+        burger.classList.remove('header__burger--line--active');
+
+        menu.classList.remove('header__nav--active');
+
+        document.body.classList.remove('header__stop--scroll');
+
+    });
+});
